a4513855852f69fb925333f233f3ff9e394185e7
AlexanderSerhiienko <alexanderserhiienko@gmail.com>
2021-03-26 12:07:55 +0200

    replaced build with dist in index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use('/', express.static(path.resolve('client', 'dist')))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve('client', 'build', 'index.html'))
+    res.sendFile(path.resolve('client', 'dist', 'index.html'))
   })
 }
 
